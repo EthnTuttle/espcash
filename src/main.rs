4a2118241db579c13fab864501deb20ee8495cd2
Ethan Tuttle <tuttle.ethan@protonmail.com>
2024-02-22 21:30:26 -0500

    it built

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{bail, Result};
-use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::Peripherals};
-use wifi::wifi;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, hal::prelude::Peripherals, hal::peripheral, wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi}};
+use std::str::FromStr;
+
+use log::info;
 
 pub struct Config {
     wifi_ssid: &'static str,
@@ -16,7 +18,7 @@ fn main() -> Result<()> {
 
     let app_config = Config {
         wifi_ssid: "wifi",
-        wifi_psk: "password"
+        wifi_psk: "password",
     };
     // Connect to the Wi-Fi network
     let _wifi = match wifi(
@@ -35,4 +37,72 @@ fn main() -> Result<()> {
         }
     };
     Ok(())
+}
+
+pub fn wifi(
+    ssid: &str,
+    pass: &str,
+    modem: impl peripheral::Peripheral<P = esp_idf_svc::hal::modem::Modem> + 'static,
+    sysloop: EspSystemEventLoop,
+) -> Result<Box<EspWifi<'static>>> {
+    let mut auth_method = AuthMethod::WPA2Personal;
+    if ssid.is_empty() {
+        bail!("Missing WiFi name")
+    }
+    if pass.is_empty() {
+        auth_method = AuthMethod::None;
+        info!("Wifi password is empty");
+    }
+    let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), None)?;
+
+    let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop)?;
+
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration::default()))?;
+
+    info!("Starting wifi...");
+
+    wifi.start()?;
+
+    info!("Scanning...");
+
+    let ap_infos = wifi.scan()?;
+
+    let ours = ap_infos.into_iter().find(|a| a.ssid == ssid);
+
+    let channel = if let Some(ours) = ours {
+        info!(
+            "Found configured access point {} on channel {}",
+            ssid, ours.channel
+        );
+        Some(ours.channel)
+    } else {
+        info!(
+            "Configured access point {} not found during scanning, will go with unknown channel",
+            ssid
+        );
+        None
+    };
+    let ssid = heapless::String::<32>::from_str(ssid).expect("bad type casting of ssid");
+    let password = heapless::String::<64>::from_str(pass).expect("bad type casting of password.");
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid,
+        password,
+        channel,
+        auth_method,
+        ..Default::default()
+    }))?;
+
+    info!("Connecting wifi...");
+
+    wifi.connect()?;
+
+    info!("Waiting for DHCP lease...");
+
+    wifi.wait_netif_up()?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+
+    info!("Wifi DHCP info: {:?}", ip_info);
+
+    Ok(Box::new(esp_wifi))
 }
